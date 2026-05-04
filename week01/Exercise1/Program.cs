83c03cba725200590e55ff82e45e9973f43c9d08
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-04 17:27:14 -0300

    Finished the 1st excercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        string firstName;
+        string lastName;
+
+        Console.WriteLine("What is your first name? ");
+        firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
     }
 }
