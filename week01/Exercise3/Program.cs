25db9e248018e104fc55d767e9fe21be1c0d85d5
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-05 17:57:47 -0300

    Work in progress Ex 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magicNumber = 7;
+        string guess = "0";
+        int numberGuessed = 0;
+
+        while (numberGuessed != magicNumber);
+        {
+            Console.WriteLine("What is the magic number? ");
+            guess = Console.ReadLine();
+            numberGuessed = int.Parse(guess);
+            Console.WriteLine($"{numberGuessed}");
+        } 
+
+        
     }
 }
