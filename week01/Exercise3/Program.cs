7758b51d2e0dbaa2f7e15a0842fd376d779cea3c
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-06 15:18:01 -0300

    Finished ex 3 (again)

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,20 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        int magicNumber = 7;
-        string guess = "0";
-        int numberGuessed = 0;
+        Random randomNum = new Random();
+        string answer = "yes";
 
-        while (true)
+        while (answer == "yes")
         {
-           Console.WriteLine("What is the magic number? ");
-           guess = Console.ReadLine();
-           numberGuessed = int.Parse(guess);
+            
+            int magicNumber = randomNum.Next(1, 100);
+            string guess = "0";
+            int numberGuessed = 0;
+            int tries = 0;
+            while (true)
+            {
+               Console.WriteLine("What is the magic number? ");
+               guess = Console.ReadLine();
+               numberGuessed = int.Parse(guess);
+               tries += 1;
 
-           if (numberGuessed == magicNumber)
+                if (numberGuessed < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (numberGuessed > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                if (numberGuessed == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Number of tries: {tries}");
+                    break;
+                } 
+            }
+
+            Console.WriteLine("Do you want to play again?");
+            answer = Console.ReadLine().ToLower();
+            if (answer == "no")
             {
                 break;
-            } 
+            }
         }
     }
-}tyyyyyyy
+}
