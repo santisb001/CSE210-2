fc3881813344d06d9652f19482b86c9906664f0e
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-06 16:29:38 -0300

    Finished ex 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            return number;
+        }
+
+        static int SquareNumber(int num)
+        {
+            int square = num * num;
+
+            return square;
+        }
+
+        static void DisplayResult(string name, int sqr)
+        {
+            Console.WriteLine($"{name}, the square of your number is {sqr}");
+        }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+       DisplayWelcome();
+       string name = PromptUserName();
+       int number = PromptUserNumber();
+       int squareNumber = SquareNumber(number);
+       DisplayResult(name,squareNumber);
     }
 }
