b478fea76a732591304fda9d075a9ece4f03dee1
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-07 15:13:38 -0300

    Finished the week 02 practice

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,19 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._company = "Mincrosoft";
-        job1._jobTitle = "Software Engeneer";
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
         job1._startYear = 2019;
         job1._endYear = 2022;
 
 
-        job1 job2 = new job();
+        Job job2 = new Job();
         job2._company = "MRoyo";
-        job2._jobTitle = "Auxiliary tressurer";
+        job2._jobTitle = "Auxiliary treasurer";
         job2._startYear = 2024;
         job2._endYear = 2030;
 
         Resume resume1 = new Resume();
+        resume1._name = "Santiago Sburlati";
+        resume1.AddJob(job1);
+        resume1.AddJob(job2);
+        resume1.Display();
         
     }
 }
