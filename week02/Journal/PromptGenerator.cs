e8d4119994b26ebbbf33f20de5d3fb4691ec10d5
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-11 15:28:56 -0300

    Finished the 1st version of the PromptGenerator Class

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,18 +1,31 @@
 using System;
 using System.Collections.Generic;
-
+using System.IO;
+using System.Linq;
 public class PromptGenerator
 {
     //Atributes
     public List<string> _listOfPrompts = new List<string>();
     public string _file = "PromptsFile.csv";
-    public string _prompt = "";
+    //public string[] _prompts = File.ReadAllLines(_file);
+    public Random _random = new Random(); 
 
     //Methods
     public string ReturnRandomPrompt()
     {
-        //Select a prompt from the list and return it as a string
-        return "Str";
+        //Select a prompt from the list and return it as a string, and make them not repeat
+        if (_listOfPrompts.Count == 0)
+        {
+            _listOfPrompts = File.ReadAllLines(_file).ToList();
+        }
+
+        int index = _random.Next(_listOfPrompts.Count);
+
+        string chosenPrompt = _listOfPrompts[index];
+
+        _listOfPrompts.RemoveAt(index);
+
+        return chosenPrompt;
     }
 
 }
