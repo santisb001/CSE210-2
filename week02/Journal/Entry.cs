1206d32c6b06bc142a5c8996fd4781528565d623
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-11 17:14:26 -0300

    Made some changes in the Entry class

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 public class Entry
 {
     //Atributes
-    public string _date = "";
+    public string _date;
     public string _inputText = "";
     public string _promptText = "";
+    public PromptGenerator _promptGeneratorChoice;
+    //Constructor
+    public Entry()
+    {
+        _date = DateTime.Now.ToShortDateString();
+        _promptGeneratorChoice = new PromptGenerator();
+        _promptText = _promptGeneratorChoice.ReturnRandomPrompt();
+    }
+    
 
     //Methods
-    public string DisplayEntry()
+    public void DisplayEntry()
     {
         //Show a string with all the info of the entry to the screen
-        Console.WriteLine($"{_promptText}: {_inputText} -- {_date}");
+        Console.WriteLine($"{_date} -- {_promptText}: {_inputText}");
     }
 
 }
