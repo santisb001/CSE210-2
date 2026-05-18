45099de47a3728b5ea3d07c01699e96d1ba0c918
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-18 17:30:56 -0300

    Finished w03 practice

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3,4);
+        Fraction fraction4 = new Fraction(1,3);
+
+        fraction1.ReturnFractionString();
+        fraction1.ReturnDecimalValue();
+
+        fraction2.ReturnFractionString();
+        fraction2.ReturnDecimalValue();
+
+        fraction3.ReturnFractionString();
+        fraction3.ReturnDecimalValue();
+
+        fraction4.ReturnFractionString();
+        fraction4.ReturnDecimalValue();
+
     }
 }
