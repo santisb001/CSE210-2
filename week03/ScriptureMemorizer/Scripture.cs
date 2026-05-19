fef2a434a00a1f99c54e42c89f35255614706bc1
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-19 15:53:57 -0300

    I created the Reference, Word and Scripture classe's files

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+public class Scripture
+{
+    
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Word
+{
+    
+}
