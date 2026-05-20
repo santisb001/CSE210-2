b8598e0e2086abc9339b8745f0f606c03b59e0d6
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-20 15:21:35 -0300

    Almost finished the Word class

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -6,35 +6,50 @@ using System.Linq;
 public class Word
 {
     //Attributes
-    private List<string> _characters;
+    private string _word;
+    private string _hiddenContent;
+    private bool _isHidden;
+    
 
 
     //Constructor
-    public Word (string characters)
+    public Word (string word)
     {
+        _word = word;
+        _hiddenContent = new string ('_', word.Length);
+        _isHidden = false;
         
     }
-
 
     //Methods
     public void Hide()
     {
-        //Hide the word    
+        //Show the hidden word  
+        Console.WriteLine($"{_hiddenContent}");
     }
 
     public void Show()
     {
         //Show the word
+        Console.WriteLine($"{_word}");
     }
 
     public bool IsHidden ()
     {
         //return that it is, in fact, hidden
-
+        return _isHidden = true;
     }
 
     public string ReturnDisplayText ()
     {
         //return a string
+        if (_isHidden == false)
+        {
+            return _word;
+        }
+        else
+        {
+            return _hiddenContent;
+        }
     }
 }
