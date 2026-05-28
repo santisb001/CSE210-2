2c2371d313ccc7e363160ebcc296474849c919b8
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-28 15:51:03 -0300

    fddf

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        using System;
+        using System.Collections.Generic;
+        using System.IO;
+        using System.Linq;
     }
 }
