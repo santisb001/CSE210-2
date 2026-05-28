687d38c2a2bf367819b74b455049d4a33627bead
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-28 17:25:21 -0300

    finished the Video and Comment classes

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -6,15 +6,21 @@ using System.Linq;
 public class Comment
 {
     //Attributes
-
-
+    private string _author;
+    private string _text;
 
     //Constructor
-
-
+    public Comment(string person, string text)
+    {
+        _author = person;
+        _text = text;
+    }
 
     //Methods
-
+    public void DisplayComment()
+    {
+        Console.WriteLine($"{_author}: {_text}");
+    }
 
     
 }
--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -6,15 +6,41 @@ using System.Linq;
 public class Video
 {
     //Attributes
-
-
+    private string _title;
+    private string _author;
+    private int _duration;
+    private List<Comment> _comments;
 
     //Constructor
-
-
+    public Video(string title, string author, int duration)
+    {
+        _title = title;
+        _author = author;
+        _duration = duration;
+        _comments = new List<Comment>();
+    }
 
     //Methods
-
-
+    public void AddComment(string commentAuthor, string commentText)
+    {
+        Comment newComment = new Comment(commentAuthor,commentText);
+        _comments.Add(newComment);
+    }
     
+    public void DisplayComments()
+    {
+        foreach(Comment C in _comments)
+        {
+            C.DisplayComment();
+        }
+    }
+
+    public int CommentQuantity()
+    {
+        int quantity = _comments.Count();
+        
+        return quantity;
+    }
+
+
 }
