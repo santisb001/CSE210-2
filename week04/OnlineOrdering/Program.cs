c575a936dd3030561f3c5e11e58698063702a4f2
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-28 17:43:24 -0300

    working in the main program of the YT video

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,7 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Video video1 = new Video("The ten most fast snails","SnailD0ct0r55",1504);
+        video1.AddComment("AracnidLover67","Spiders are way cooler than snails, a snail would totally lose against any spider");
+        video1.AddComment("XxMaineCraftxX","@AracnidLover67 chill bro, you ain't a spider. Also, snails are delicious!");
+        video1.AddComment("BravoMacho","I'm terrified of snails");
         
+        Video video2 = new Video("How to fix your","SnailD0ct0r55",1504);
+        video2.AddComment("AracnidLover67","Spiders are way cooler than snails, a snail would totally lose against any spider");
+        video2.AddComment("XxMaineCraftxX","@AracnidLover67 chill bro, you ain't a spider. Also, snails are delicious!");
+        video2.AddComment("BravoMacho","I'm terrified of snails");
+
+        Video video3 = new Video("The ten most fast snails","SnailD0ct0r55",1504);
+        video3.AddComment("AracnidLover67","Spiders are way cooler than snails, a snail would totally lose against any spider");
+        video3.AddComment("XxMaineCraftxX","@AracnidLover67 chill bro, you ain't a spider. Also, snails are delicious!");
+        video3.AddComment("BravoMacho","I'm terrified of snails");
+
+        Video video4 = new Video("The ten most fast snails","SnailD0ct0r55",1504);
+        video4.AddComment("AracnidLover67","Spiders are way cooler than snails, a snail would totally lose against any spider");
+        video4.AddComment("XxMaineCraftxX","@AracnidLover67 chill bro, you ain't a spider. Also, snails are delicious!");
+        video4.AddComment("BravoMacho","I'm terrified of snails");
     }
 }
