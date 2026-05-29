259037287277b3e5e6fdc6a7ba201e6c226e09f7
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-29 16:24:07 -0300

    worked on the Product and Order classes

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -6,13 +6,34 @@ using System.Linq;
 public class Order
 {
     //Attributes
-
+    private List<Product> _products;
+    private Customer _customer;
+    private bool _inUSA;
 
     //Constructor
-
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+        _inUSA = _customer.InsideTheUSA();
+    }
 
     //Methods
+    public void AddProduct(string nameProd, string ID, int Q, double price)
+    {
+        Product newProduct = new Product(nameProd, ID, Q, price);
+        _products.Add(newProduct);
 
+    }
 
-    
+    public string ShippingLabel()
+    {
+        //return a string with the name and address of the customer
+    }
+
+    public void PackingLabel()
+    {
+        //return a string with the name and ID of each product in the order
+    }
+
 }
--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -6,13 +6,28 @@ using System.Linq;
 public class Product
 {
     //Attributes
-
-
+    private string _prodName;
+    private string _prodID;    
+    private int _quantity;
+    private double _price;
+    
     //Constructor
-
+    public Product(string name, string id, int quantity, double price)
+    {
+        _prodName = name;
+        _prodID = id;
+        _quantity = quantity;
+        _price = price;
+    }
 
     //Methods
+    public double ProdCost()
+    {
+        //Calculate total cost of the product
+        double cost = _price * _quantity;
 
+        return cost;
+    }
 
     
 }
