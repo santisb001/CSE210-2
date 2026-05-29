9548445ea0afbac34fa24d3ff1c1a675fe95d221
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-05-29 15:55:33 -0300

    Worked on the Address and Customer classes

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -6,12 +6,35 @@ using System.Linq;
 public class Address
 {
     //Attributes
-
+    private string _street;
+    private string _city;
+    private string _stateProvince;
+    private string _country;
+    private bool _inUSA;
 
     //Constructor
-
+    public Address(string street, string city, string state, string country)
+    {
+        _street = street;
+        _city = city;
+        _stateProvince = state;
+        _country = country;
+        _inUSA = true;
+    }
 
     //Methods
+    public void OutsideTheUS()
+    {
+        _inUSA = false;
+    }
+    public bool InTheUS()
+    {
+        return _inUSA;
+    }
+    public string MakeAddress()
+    {
+        return $"{_street}, {_city}, {_stateProvince}, {_country}";
+    }
 
 
     
--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -6,12 +6,25 @@ using System.Linq;
 public class Customer
 {
     //Attributes
-
+    private string _name;
+    private Address _address;
 
     //Constructor
-
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
 
     //Methods
+    public bool InsideTheUSA()
+    {
+        return _address.InTheUS();
+    }
+
+    
+
+
 
 
     
