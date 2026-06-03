47657f277125879c0630f4a8c70853d3bef0ca5c
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-06-03 17:58:07 -0300

    Finished the set up for all the derived classes

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -6,10 +6,41 @@ using System.Linq;
 public class ListingActivity : Activity
 {
     //Attributes
-
+    private List<string> _prompts;
+    private int _count;
 
     //Constructor
-
+    public ListingActivity(string start, string end, string description) : base (start,end,description)
+    {
+        _prompts = new List<string>();
+        _count = 0;
+    }
 
     //Methods
+    public void Run()
+    {
+        //code the execution of the class
+        ///
+        /// 
+        /// 
+        /// Finish later with the loop
+        /// 
+        /// 
+        /// 
+    }
+
+    ///
+    /// 
+    /// public void GetPrompt()
+    /// {
+    ///     //not sure how to use this one
+    /// }
+    /// 
+    /// 
+    /// public GetListFromUser()
+    /// {
+    ///     //return List<string>
+    /// }
+    /// 
+    /// 
 }
--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -6,10 +6,40 @@ using System.Linq;
 public class ReflectionActivity : Activity
 {
     //Attributes
-
+    private List<string> _prompts;
+    private List<string> _reflections;
+    Random _random;
 
     //Constructor
-
+    public ReflectionActivity(string start, string end, string description) : base (start,end,description)
+    {
+        _prompts = new List<string>();
+        _reflections = new List<string>();
+        _random = new Random();
+    }
 
     //Methods
+    public void Run()
+    {
+        //code the execution of the class
+        ///
+        /// 
+        /// 
+        /// Finish later with the loop
+        /// 
+        /// 
+        /// 
+    }
+    
+    public string GetPrompt()
+    {
+        //Select and return a random prompt
+        //return string
+    }
+
+    public string GetReflection()
+    {
+        //Select and return a random Reflection
+        //return string
+    } 
 }
