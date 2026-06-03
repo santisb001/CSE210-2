bb180e9ebec5871514447d45f2e18b59a2d4cbe5
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-06-03 11:56:21 -0300

    Finished the Math and Writing derived classes

--- week05/Homework/MathAssignment.cs
+++ week05/Homework/MathAssignment.cs
@@ -6,8 +6,21 @@ using System.Linq;
 public class MathAssignment : Assignment
 {
     //Attributes
-    
+    private string _textBookSection;
+    private string _problems;
+
     //Constructor
-    
+    public MathAssignment(string name, string topic, string section, string problems) : base(name,topic)
+    {
+        _textBookSection = section;
+        _problems = problems;
+    }
+
     //Methods
+    public string GetHomeworkList()
+    {
+        return $"Section {_textBookSection} Problems {_problems}";
+    }
+
+
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class WritingAssignment : Assignment
+{
+    //Attributes
+    private string _title;
+
+    //Constructor
+    public WritingAssignment(string name, string topic, string title)
+    {
+        _title = title;
+    }
+
+    //Methods
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetName()}";
+    }
+
+
+}
