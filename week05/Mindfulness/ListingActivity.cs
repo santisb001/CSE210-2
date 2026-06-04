cb291e0bcf7fbddbc80c81df2dde202ca27dcdda
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-06-04 16:13:51 -0300

    preparing to keep going

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -31,16 +31,19 @@ public class ListingActivity : Activity
 
     ///
     /// 
-    /// public void GetPrompt()
-    /// {
-    ///     //not sure how to use this one
-    /// }
-    /// 
-    /// 
-    /// public GetListFromUser()
-    /// {
+    public void GetPrompt()
+    {
+        /////not sure how to use this one
+        ///Who are people that you appreciate?
+        ///What are personal strengths of yours?
+        ///Who are people that you have helped this week?
+        ///When have you felt the Holy Ghost this month?
+        ///Who are some of your personal heroes?
+    }
+     
+    public List<string> GetListFromUser()
+    {
     ///     //return List<string>
-    /// }
-    /// 
-    /// 
+    }
+ 
 }
--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -34,12 +34,31 @@ public class ReflectionActivity : Activity
     public string GetPrompt()
     {
         //Select and return a random prompt
+        ///
+        /// 
+        ///Think of a time when you stood up for someone else.
+        ///Think of a time when you did something really difficult.
+        ///Think of a time when you helped someone in need.
+        ///Think of a time when you did something truly selfless.   
+        /// 
+        /// 
         //return string
     }
 
     public string GetReflection()
     {
         //Select and return a random Reflection
+        ///
+        ///Why was this experience meaningful to you?
+        ///Have you ever done anything like this before?
+        ///How did you get started?
+        ///How did you feel when it was complete?
+        ///What made this time different than other times when you were not as successful?
+        ///What is your favorite thing about this experience?
+        ///What could you learn from this experience that applies to other situations?
+        ///What did you learn about yourself through this experience?
+        ///How can you keep this experience in mind in the future?
+        ///
         //return string
     } 
 }
