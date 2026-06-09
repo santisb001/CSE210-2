344c2623605a06e7c51c05ba261259b0f8f52a29
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-06-09 17:39:42 -0300

    Createds the bases for the practice w06 exercise

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+//You may have to make it an ABSTRACT or INTERFACE
+public class Shape
+{
+    //Attributes
+
+    //Constructor
+
+    //Methods
+}
