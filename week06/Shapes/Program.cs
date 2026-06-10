c94c40c55509e37e2be0ddb9fd97193172702e8c
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-06-10 12:29:18 -0300

    Finished the practice exercise of w06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,10 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        //using System;
-        //using System.Collections.Generic;
-        //using System.IO;
-        //using System.Linq;
-        //using System.Threading;   
+        Square square1 = new Square("Saquare","Green",4.5);
+        Rectangle rectangle1 = new Rectangle("Rectangle","Yellow",4,5.9);
+        Circle circle1 = new Circle("Circle","Red",7.8);
+
+        List <Shape> shapes = new List<Shape>();
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+
+        foreach (Shape S in shapes)
+        {
+            string name = S.GetName();
+            string color = S.GetColor();
+            double area = S.GetArea();
+
+            Console.WriteLine($"{name}, {color}, {area}");
+
+        }
     }
 }
